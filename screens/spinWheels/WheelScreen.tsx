456f6f0d8297888ae5782f60b026f149b3ef1c32
Amarasystems <amarasystem.tech@gmail.com>
2025-01-10 23:25:55 +0800

    fix design spin

--- screens/spinWheels/WheelScreen.tsx
+++ screens/spinWheels/WheelScreen.tsx
@@ -7,11 +7,13 @@ import {
   TouchableOpacity,
   Modal,
   Animated,
+  ActivityIndicator,
 } from "react-native";
 import { SERVER_URI } from "@/utils/uri";
 import axios from "axios";
 import Colors from "@/constants/Colors";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
 
 type PrizeDetails = {
   prizeName: string;
@@ -25,11 +27,9 @@ export default function WheelScreen() {
   const [prizeImage, setPrizeImage] = useState<string>("");
   const [prize, setPrize] = useState<string>("");
   const [errorModal, setErrorModal] = useState<string | null>(null);
-  const [lottoCount, setLottoCount] = useState<number>();
+  const [lottoCount, setLottoCount] = useState<number>(0);
   const [token, setToken] = useState<string>("");
-
   const circleRef = useRef<any>(null);
-
   const spinValue = useRef(new Animated.Value(0)).current;
 
   const getPrizeDetails = (giftName: string): PrizeDetails => {
@@ -62,7 +62,6 @@ export default function WheelScreen() {
   const checkLotto = async (): Promise<void> => {
     try {
       const access_token = await AsyncStorage.getItem("token");
-
       if (!access_token) {
         console.log("Access token not found");
         return;
@@ -70,7 +69,6 @@ export default function WheelScreen() {
       const response = await axios.get(`${SERVER_URI}/api/user`, {
         headers: { Authorization: `Bearer ${access_token}` },
       });
-
       const lottoCount = response.data.response.lottoCount;
       setLottoCount(lottoCount);
     } catch (error) {
@@ -82,21 +80,20 @@ export default function WheelScreen() {
     checkLotto();
   }, []);
 
-  const handlespin = async (): Promise<void> => {
-    checkLotto();
+  const handleSpin = async (): Promise<void> => {
+    if (isSpinning) return;
 
     setLoader(true);
     setIsSpinning(true);
 
-    const access_token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkNVZ0M1aGJXWjNsMzJRS0hmdDgyIiwiaWF0IjoxNzM2MzI1MTk3LCJleHAiOjE3MzY0MTE1OTd9.ep9ivv_iYN8Jus3QWkrQyHO0V9CyVUy5vXgfunRMBu8";
-
+    const access_token = await AsyncStorage.getItem("token");
     if (access_token) {
       try {
         if (lottoCount === 0) {
           setErrorModal("Таньд хүрд эргүүлэх эрх байхгүй байна.");
           setTimeout(() => setErrorModal(null), 2000);
           setLoader(false);
+          setIsSpinning(false);
           return;
         }
 
@@ -111,10 +108,8 @@ export default function WheelScreen() {
           const prizeDetails = getPrizeDetails(giftName);
           const prizeDeg = prizeDetails.deg;
 
-          // Reset the spin value before starting the animation (only when new spin starts)
           spinValue.setValue(0);
 
-          // Animate the spinning
           Animated.timing(spinValue, {
             toValue: spinValue._value + 360 + prizeDeg,
             duration: 4500,
@@ -131,6 +126,7 @@ export default function WheelScreen() {
         } else {
           throw new Error("Invalid response from server");
         }
+
         setLoader(false);
       } catch (error: any) {
         setIsSpinning(false);
@@ -154,25 +150,33 @@ export default function WheelScreen() {
     setPrize("");
     setPrizeImage("");
     setErrorModal("");
-
-    // Reset the spin value to 0 so the next spin starts fresh
     spinValue.setValue(0);
+  };
+
+  const handleProfile = () => {
+    router.push("/(routes)/profile");
   };
 
   return (
     <View style={styles.container}>
-      {/* <View style={styles.containersmall}>
-        <Image
-          source={require("@/assets/loyalty/zuun.png")} // Adjust the path
-          style={styles.choket}
-        />
-        <Image
-          source={require("@/assets/loyalty/baruun.png")} // Adjust the path
-          style={styles.choket}
-        />
-      </View> */}
+      <Text
+        style={{ color: "white", position: "absolute", top: 70, right: 30 }}
+        onPress={handleProfile}
+      >
+        Хаах
+      </Text>
       <View>
-        <TouchableOpacity onPress={handlespin}>
+        <TouchableOpacity onPress={handleSpin} disabled={isSpinning}>
+          <View style={styles.containersmall}>
+            <Image
+              source={require("@/assets/loyalty/zuun.png")}
+              style={styles.choket}
+            />
+            <Image
+              source={require("@/assets/loyalty/baruun.png")}
+              style={styles.choket}
+            />
+          </View>
           <Image
             source={require("@/assets/loyalty/bg.png")}
             style={styles.bg}
@@ -198,10 +202,11 @@ export default function WheelScreen() {
             ]}
           />
         </TouchableOpacity>
+        <Text style={styles.lotto}>
+          Таньд {lottoCount} хүрд эргүүлэх эрх байна.
+        </Text>
       </View>
-
       {errorModal && <Text style={styles.errorText}>{errorModal}</Text>}
-
       <Modal
         transparent={true}
         animationType="fade"
@@ -224,64 +229,52 @@ export default function WheelScreen() {
           </View>
         </View>
       </Modal>
-
-      {loader && (
-        <View>
-          <Text>loading</Text>
-        </View>
-      )}
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center", // Centers vertically
-    alignItems: "center", // Centers horizontally
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: Colors.black,
-    paddingHorizontal: 20, // Add padding for smaller devices
+    paddingHorizontal: 20,
+  },
+  bg: { width: 500, height: 500, position: "relative", resizeMode: "contain" },
+  red: {
+    width: 25,
+    height: 25,
+    position: "absolute",
+    top: "17%",
+    left: "50%",
+    marginLeft: -12,
+    marginTop: -12,
   },
   containersmall: {
     flexDirection: "row", // Layout the images horizontally (side by side)
     justifyContent: "center", // Center images within the containersmall
     alignItems: "center",
     position: "absolute",
+    top: -50,
+    left: -100,
+    zIndex: 1,
   },
   choket: {
     width: 400, // Set the desired width of the images
     height: 600, // Set the desired height of the images
   },
-
-  bg: {
-    width: 500,
-    height: 500,
-    position: "relative",
-    resizeMode: "contain", // Ensure it's properly scaled on various devices
-  },
-  red: {
-    width: 25,
-    height: 25,
-    position: "absolute",
-    top: "17%", // Adjust to keep it inside the spinning circle
-    left: "50%",
-    marginLeft: -12, // To center the small red circle exactly
-    marginTop: -12, // To center it vertically
-  },
   spin: {
     width: 300,
     height: 300,
     position: "absolute",
-    zIndex: 3,
+    zIndex: 10,
     top: "20%",
     left: "20%",
-    transform: [{ translateX: -150 }, { translateY: -150 }], // Center the wheel exactly
+    transform: [{ translateX: -150 }, { translateY: -150 }],
   },
-
-  errorText: {
-    color: "white",
-    textAlign: "center",
-    marginVertical: 10, // Space out the error message
-  },
+  lotto: { color: "#ffffff", position: "absolute", top: 415, left: 114 },
+  errorText: { color: "white", textAlign: "center", marginVertical: 10 },
   modalOverlay: {
     flex: 1,
     justifyContent: "center",
@@ -293,24 +286,16 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 10,
     alignItems: "center",
-    width: "80%", // Ensure modal content is responsive
-    maxWidth: 400, // Limit the max width on larger screens
+    width: "80%",
+    maxWidth:300
   },
-  modalTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 10, // Space between title and prize name
-  },
-  modalPrize: {
-    fontSize: 18,
-    marginVertical: 10,
-    color: Colors.black, // Highlight the prize with a custom color
-  },
+  modalTitle: { fontSize: 24, fontWeight: "bold", marginBottom: 10 },
+  modalPrize: { fontSize: 18, marginVertical: 10, color: Colors.black },
   prizeImage: {
     width: 100,
     height: 100,
     marginVertical: 10,
-    resizeMode: "contain", // Ensure the prize image doesn't stretch
+    resizeMode: "contain",
   },
   closeButton: {
     marginTop: 20,
@@ -319,19 +304,6 @@ const styles = StyleSheet.create({
     backgroundColor: "rgb(59 130 246)",
     borderRadius: 5,
   },
-  closeButtonText: {
-    color: "white",
-    fontSize: 16,
-  },
-  loaderContainer: {
-    position: "absolute",
-    top: "50%", // Center the loader in the screen
-    left: "50%",
-    transform: [{ translateX: -25 }, { translateY: -25 }],
-  },
-  loaderText: {
-    color: "white",
-    fontSize: 16,
-    marginTop: 10, // Space out the loader text
-  },
+  closeButtonText: { color: "white", fontSize: 16 },
+  loaderContainer: { flex: 1, justifyContent: "center", alignItems: "center" },
 });
