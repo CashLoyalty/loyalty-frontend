8f71051e17e1816f7b77c192ebc60aa33532abb0
Bat-Orgil.S <64575871+Sw09d240@users.noreply.github.com>
2025-04-03 01:14:17 +0800

    Add ADF

--- app/(tabs)/award/index.tsx
+++ app/(tabs)/award/index.tsx
@@ -55,6 +55,40 @@ const products = [
   { id: "4", image: require("@/assets/icons/headPhone.png") },
 ];
 
+interface PrizeItem {
+  id: string;
+  title: string;
+  score: number;
+  image: any;
+}
+
+const prizes: PrizeItem[] = [
+  {
+    id: "1",
+    title: "Burger King 20`000₮ эрхийн бичиг",
+    score: 15000,
+    image: require("@/assets/icons/bur20.png"),
+  },
+  {
+    id: "2",
+    title: "Азын хүрд эргүүлэх",
+    score: 15000,
+    image: require("@/assets/icons/bur20.png"),
+  },
+  {
+    id: "3",
+    title: "Prime Cineplex 20`000₮ эрхийн бичиг",
+    score: 20000,
+    image: require("@/assets/icons/bur20.png"),
+  },
+  {
+    id: "4",
+    title: "PIZZA HUT 15`000 эрхийн бичиг",
+    score: 15000,
+    image: require("@/assets/icons/bur20.png"),
+  },
+];
+
 const Award: React.FC = () => {
   console.log("width : " + width + " height : " + height);
   const handleBackPress = () => {
@@ -101,6 +135,10 @@ const Award: React.FC = () => {
     </View>
   );
 
+  const handleItemPress = (item: PrizeItem) => {
+    console.log("item TTT : " + item.id);
+  };
+
   return (
     <View style={styles.container}>
       <Header />
@@ -128,8 +166,33 @@ const Award: React.FC = () => {
         />
       </View>
       <View style={styles.container2}>
-        <Text style={styles.wheelTitle}>Point Market</Text>
+        <Text style={styles.wheelTitle}>Point Market</Text>``
       </View>
+      <FlatList
+        data={prizes}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+        columnWrapperStyle={styles.row}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => handleItemPress(item)}>
+            <View style={styles.prizeContainer}>
+              <View style={styles.prizeImgContainer}>
+                <Image source={item.image} style={styles.prizeImage} />
+              </View>
+              <View style={styles.prizeInfoContainer}>
+                <Text style={styles.prizeInfoTitle}>{item.title}</Text>
+                <View style={styles.prizeScoreContainer}>
+                  <Text style={styles.prizeScoreTitle}>{item.score}</Text>
+                  <Image
+                    source={require("@/assets/icons/plug.png")}
+                    style={{ width: 14, height: 16 }}
+                  />
+                </View>
+              </View>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
       {/* <View style={styles.emptyImageContainer}>
         <Image
           source={require("@/assets/images/emptyAward.png")}
@@ -186,8 +249,6 @@ const styles = StyleSheet.create({
     bottom: 0,
   },
   emptyImage: {
-    //width: width / 100 * 62.5,
-    //height: height / 100 * 41,
     width: 255,
     height: 291,
     opacity: 0.5,
@@ -270,6 +331,87 @@ const styles = StyleSheet.create({
     height: 60,
     borderRadius: 30,
     resizeMode: "contain",
+  },
+  prizeContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  prizeImgContainer: {
+    zIndex: 2,
+    width: 161,
+    height: 103,
+    borderWidth: 1,
+    borderColor: Colors.primaryColor,
+    borderRadius: 10,
+    backgroundColor: Colors.white,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  prizeImage: {
+    width: 150,
+    height: 70,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  prizeInfoContainer: {
+    flexDirection: "column",
+    width: 161,
+    marginTop: -20,
+    height: 100,
+    backgroundColor: Colors.giftBackgroundColor,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: Colors.primaryColor,
+    zIndex: 1,
+    paddingTop: 10,
+  },
+  giftInfoTitle: {
+    fontSize: 13,
+    fontWeight: "600",
+    color: Colors.white,
+    marginLeft: 20,
+  },
+  scoreContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    marginLeft: 20,
+    marginTop: 25,
+    marginBottom: 10,
+  },
+  questionContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    marginLeft: 20,
+  },
+  prizeInfoTitle: {
+    fontSize: 12,
+    fontWeight: "600",
+    color: Colors.white,
+    paddingTop: 20,
+    paddingLeft: 10,
+  },
+  prizeScoreContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginTop: 5,
+    marginRight: 20,
+  },
+  prizeScoreTitle: {
+    fontSize: 10,
+    fontWeight: "600",
+    color: Colors.white,
+    marginTop: 5,
+    marginRight: 5,
+  },
+  row: {
+    justifyContent: "space-between", // Spaces columns evenly
+    paddingHorizontal: 10, // Adjust padding
+    paddingVertical: 10,
   },
 });
 
