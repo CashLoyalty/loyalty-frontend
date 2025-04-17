fc7d18fedeba069b4c40438f28fe8f3ea1a48413
Amarasystems <amarasystem.tech@gmail.com>
2025-04-17 18:01:42 +0800

    g

--- app/(routes)/qrcodeReader/index.tsx
+++ app/(routes)/qrcodeReader/index.tsx
@@ -2,5 +2,6 @@ import React from "react";
 import QrcodeReaderScreen from "@/screens/qrcodeReader/QrcodeReaderScreen";
 
 export default function QrCodeReader() {
-  return ;
+  return;
+  <QrcodeReaderScreen />;
 }
