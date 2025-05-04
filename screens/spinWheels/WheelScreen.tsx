e2a1d76017e35d867c1a8e65a9d75088fc1ee537
Amarasystems <nakii@Nakiis-MacBook-Air-M4.local>
2025-05-04 16:50:46 +0800

    fix spin

--- screens/spinWheels/WheelScreen.tsx
+++ screens/spinWheels/WheelScreen.tsx
@@ -1,19 +1,11 @@
 import React, { useRef, useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-  Modal,
-  Animated,
-  ActivityIndicator,
-} from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity, Modal, Animated, ActivityIndicator } from "react-native";
 import { SERVER_URI } from "@/utils/uri";
 import axios from "axios";
 import Colors from "@/constants/Colors";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
+import { useToast } from "react-native-toast-notifications";
 
 type PrizeDetails = {
   prizeName: string;
@@ -26,28 +18,22 @@ export default function WheelScreen() {
   const [modal, setModal] = useState<boolean>(false);
   const [prizeImage, setPrizeImage] = useState<string>("");
   const [prize, setPrize] = useState<string>("");
-  const [errorModal, setErrorModal] = useState<string | null>(null);
   const [lottoCount, setLottoCount] = useState<number>(0);
   const [token, setToken] = useState<string>("");
   const circleRef = useRef<any>(null);
   const spinValue = useRef(new Animated.Value(0)).current;
+  const toast = useToast();
 
   const getPrizeDetails = (giftName: string): PrizeDetails => {
     const prizeData = new Map<string, PrizeDetails>([
       ["THANK YOU", { prizeName: "Баярлалаа дахин оролдоно уу.", deg: 1313 }],
       ["IPHONE 16 PRO MAX", { prizeName: "iPhone 16 Pro Max", deg: 1349 }],
       ["AIRPODS MAX", { prizeName: "AirPods Max", deg: 1278 }],
-      [
-        "PEPSI 3 MONTHS OF USE",
-        { prizeName: "PEPSI 3 САРЫН ХЭРЭГЛЭЭ", deg: 1242 },
-      ],
+      ["PEPSI 3 MONTHS OF USE", { prizeName: "PEPSI 3 САРЫН ХЭРЭГЛЭЭ", deg: 1242 }],
       ["AIR PURIFIER", { prizeName: "АГААР ЦЭВЭРШҮҮЛЭГЧ", deg: 1206 }],
       ["SMART TV", { prizeName: "Ухаалаг зурагт", deg: 1170 }],
       ["MARSHALL SPEAKER", { prizeName: "MARSHALL SPEAKER", deg: 1134 }],
-      [
-        "PEPSI 1 MONTHS OF USE",
-        { prizeName: "PEPSI 1 САРЫН ХЭРЭГЛЭЭ", deg: 1459 },
-      ],
+      ["PEPSI 1 MONTHS OF USE", { prizeName: "PEPSI 1 САРЫН ХЭРЭГЛЭЭ", deg: 1459 }],
       ["DYSON V15", { prizeName: "Dyson V15", deg: 1421 }],
       ["APPLE WATCH", { prizeName: "Apple Watch", deg: 1385 }],
     ]);
@@ -89,17 +75,20 @@ export default function WheelScreen() {
     if (access_token) {
       try {
         if (lottoCount === 0) {
-          setErrorModal("Таньд хүрд эргүүлэх эрх байхгүй байна.");
-          setTimeout(() => setErrorModal(null), 2000);
+          toast.show("Таньд хүрд эргүүлэх эрх байхгүй байна.", {
+            type: "danger",
+            placement: "top",
+          });
           setLoader(false);
           setIsSpinning(false);
           return;
         } else if (lottoCount > 0) {
           const spinResponse = await axios.post(
-            `${SERVER_URI}/api/gift/spin`,
+            `${SERVER_URI}/api/user/spin`,
             {},
             { headers: { Authorization: `Bearer ${access_token}` } }
           );
+          console.log(spinResponse);
           if (spinResponse && spinResponse.data && spinResponse.data.response) {
             const giftName = spinResponse.data.response.name;
             const prizeDetails = getPrizeDetails(giftName);
@@ -108,7 +97,7 @@ export default function WheelScreen() {
             spinValue.setValue(0);
 
             Animated.timing(spinValue, {
-              toValue: 360 + prizeDeg, // Calculate the target value directly
+              toValue: 360 + prizeDeg,
               duration: 4500,
               useNativeDriver: true,
             }).start();
@@ -129,16 +118,23 @@ export default function WheelScreen() {
         setIsSpinning(false);
         setLoader(false);
         if (error.message === "Invalid response from server") {
-          setErrorModal("Таньд хүрд эргүүлэх эрх байхгүй байна.");
+          toast.show("Таньд хүрд эргүүлэх эрх байхгүй байна.", {
+            type: "danger",
+            placement: "top",
+          });
         } else {
-          setErrorModal("Алдаа гарлаа. Дахин оролдож үзнэ үү.");
+          toast.show("Алдаа гарлаа. Дахин оролдож үзнэ үү.", {
+            type: "danger",
+            placement: "top",
+          });
         }
-        setTimeout(() => setErrorModal(""), 2000);
       }
     } else {
       setLoader(false);
-      setErrorModal("Та эхлээд нэвтэрнэ үү.");
-      setTimeout(() => setErrorModal(""), 2000);
+      toast.show("Та эхлээд нэвтэрнэ үү.", {
+        type: "danger",
+        placement: "top",
+      });
     }
   };
 
@@ -146,7 +142,6 @@ export default function WheelScreen() {
     setModal(false);
     setPrize("");
     setPrizeImage("");
-    setErrorModal("");
     spinValue.setValue(0);
   };
 
@@ -156,32 +151,17 @@ export default function WheelScreen() {
 
   return (
     <View style={styles.container}>
-      <Text
-        style={{ color: "white", position: "absolute", top: 70, right: 30 }}
-        onPress={handleProfile}
-      >
+      <Text style={{ color: "white", position: "absolute", top: 70, right: 30 }} onPress={handleProfile}>
         Хаах
       </Text>
       <View>
         <TouchableOpacity onPress={handleSpin} disabled={isSpinning}>
           <View style={styles.containersmall}>
-            <Image
-              source={require("@/assets/loyalty/zuun.png")}
-              style={styles.choket}
-            />
-            <Image
-              source={require("@/assets/loyalty/baruun.png")}
-              style={styles.choket}
-            />
+            <Image source={require("@/assets/loyalty/zuun.png")} style={styles.choket} />
+            <Image source={require("@/assets/loyalty/baruun.png")} style={styles.choket} />
           </View>
-          <Image
-            source={require("@/assets/loyalty/bg.png")}
-            style={styles.bg}
-          />
-          <Image
-            source={require("@/assets/loyalty/red.png")}
-            style={styles.red}
-          />
+          <Image source={require("@/assets/loyalty/bg.png")} style={styles.bg} />
+          <Image source={require("@/assets/loyalty/red.png")} style={styles.red} />
           <Animated.Image
             source={require("@/assets/loyalty/spin.png")}
             style={[
@@ -199,28 +179,15 @@ export default function WheelScreen() {
             ]}
           />
         </TouchableOpacity>
-        <Text style={styles.lotto}>
-          Таньд {lottoCount} хүрд эргүүлэх эрх байна.
-        </Text>
+        <Text style={styles.lotto}>Таньд {lottoCount} хүрд эргүүлэх эрх байна.</Text>
       </View>
-      {errorModal && <Text style={styles.errorText}>{errorModal}</Text>}
-      <Modal
-        transparent={true}
-        animationType="fade"
-        visible={modal}
-        onRequestClose={() => setModal(false)}
-      >
+      <Modal transparent={true} animationType="fade" visible={modal} onRequestClose={() => setModal(false)}>
         <View style={styles.modalOverlay}>
           <View style={styles.modalContent}>
             <Text style={styles.modalTitle}>Congratulations!</Text>
             <Text style={styles.modalPrize}>{prize}</Text>
-            {prizeImage && (
-              <Image source={{ uri: prizeImage }} style={styles.prizeImage} />
-            )}
-            <TouchableOpacity
-              onPress={handleModalClose}
-              style={styles.closeButton}
-            >
+            {prizeImage && <Image source={{ uri: prizeImage }} style={styles.prizeImage} />}
+            <TouchableOpacity onPress={handleModalClose} style={styles.closeButton}>
               <Text style={styles.closeButtonText}>Хаах</Text>
             </TouchableOpacity>
           </View>
@@ -249,17 +216,17 @@ const styles = StyleSheet.create({
     marginTop: -12,
   },
   containersmall: {
-    flexDirection: "row", // Layout the images horizontally (side by side)
-    justifyContent: "center", // Center images within the containersmall
+    flexDirection: "row",
+    justifyContent: "center",
     alignItems: "center",
     position: "absolute",
-    top: -50,
-    left: -85,
+    top: 0,
+    left: -45,
     zIndex: 1,
   },
   choket: {
-    width: 400, // Set the desired width of the images
-    height: 600, // Set the desired height of the images
+    width: 340,
+    height: 500,
   },
   spin: {
     width: 300,
@@ -270,7 +237,13 @@ const styles = StyleSheet.create({
     left: "20%",
     transform: [{ translateX: -150 }, { translateY: -150 }],
   },
-  lotto: { color: "#ffffff", position: "absolute", top: 415, left: 114 },
+  lotto: {
+    color: "#ffffff",
+    position: "absolute",
+    top: 415,
+    left: "41%",
+    transform: [{ translateX: -50 }],
+  },
   errorText: { color: "white", textAlign: "center", marginVertical: 10 },
   modalOverlay: {
     flex: 1,
